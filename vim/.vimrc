0a91749eea7f221176a83d9558b42129fd9ba318
AndyLinnnn <andylin950611@gmail.com>
2024-12-06 21:27:46 +0800

    update

---
+++ vim/.vimrc
@@ -0,0 +1,46 @@
+"call plug#begin('~/.vim/plugged')
+
+"show line number
+set nu
+
+"relative number
+"set relativenumber
+
+"set cursorline
+
+"syntax highlighting
+syntax on
+
+"indentation according to different file type
+filetype indent on
+
+"automatic indentation
+set autoindent
+
+"smart indentation
+set smartindent
+
+"amount of spaces for a tab
+set tabstop=4
+
+"amount of spaces for text shifting
+set shiftwidth=4
+
+"vim spelling checker
+set nospell
+
+"avoid breaking a word when wrapping lines
+set linebreak
+
+"wrapped text remains indentation
+set bri
+
+"highlight search results
+set hlsearch
+
+"ignore cases when searching
+set ignorecase
+
+"Plug 'lervag/vimtex'
+
+"call plug#end()
---
+++ vim/init.lua
@@ -0,0 +1,49 @@
+
+--show line number
+vim.opt.nu = true
+
+--set theme
+vim.cmd("colorscheme sorbet")
+
+--relative number
+vim.opt.relativenumber = true
+
+--set cursorline
+
+--show both nu & rnu
+vim.o.statuscolumn = "%s %l %r "
+
+
+--syntax highlighting
+vim.cmd("syntax on")
+
+--indentation according to different file type
+vim.cmd("filetype indent on")
+
+--automatic indentation
+vim.opt.autoindent = true
+
+--smart indentation
+vim.opt.smartindent = true
+
+--amount of spaces for a tab
+vim.opt.tabstop = 4
+
+--amount of spaces for text shifting
+vim.opt.shiftwidth = 4
+
+--vim spelling checker
+vim.opt.spell = false
+
+--avoid breaking a word when wrapping lines
+vim.opt.linebreak = true
+
+--wrapped text remains indentation
+vim.opt.bri = true
+
+--highlight search results
+vim.opt.hlsearch = true
+
+--ignore cases when searching
+vim.opt.ignorecase = true
+
